ae997fd7923e4d43d125c5a0224a3aa59df3c47d
Mahmoud Taha <132286980+MahmoudMonis@users.noreply.github.com>
2023-05-20 01:33:30 +0300

    Finished

---
+++ Controllers/Employeecontroller.cs
@@ -0,0 +1,68 @@
+using APIfinish.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIfinish.Controllers;
+
+
+[ApiController]
+
+[Route("[controller]/[action]")]
+public class EmployeeController : ControllerBase
+{
+            private static List<Employee> employees = new List<Employee>()
+        {
+            new Employee { Id = 1, Name = "Mahmoud", Department = "HR" },
+            new Employee { Id = 2, Name = "Ibrahim", Department = "IT" },
+            new Employee { Id = 3, Name = "Sobhi", Department = "Finance" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> GetAllEmployees()
+        {
+            return employees;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetEmployeeById(int id)
+        {
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> ADDEmployee(Employee employee)
+        {
+            employee.Id = employees.Count + 1;
+            employees.Add(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id, Employee employee)
+        {
+            var existingEmployee = employees.FirstOrDefault(e => e.Id == id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+            existingEmployee.Name = employee.Name;
+            existingEmployee.Department = employee.Department;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault (e=> e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employees.Remove(employee);
+            return NoContent();
+        }
+}
